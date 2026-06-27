2ff5826648bc1e3aa83bc08535c49c4b70e1d996
DanArcherOnline <danarcheronline@gmail.com>
2026-06-27 23:01:28 +1000

    fix fe

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,12 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@dddforum/shared": path.resolve(__dirname, "../shared"),
+    },
+  },
 });
